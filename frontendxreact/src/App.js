9f69c341864bf9f7bad36ee53edf0830293814b1
JuanCarlosPerezSanchez <152633822+JuanCarlosPerezSanchez@users.noreply.github.com>
2025-02-17 20:36:19 +0100

    C

--- frontendxreact/src/App.js
+++ frontendxreact/src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
+import Button from './components/Button';
 
 function App() {
   return (
     <div className="App">
       <h1>Tarea Despliegue</h1>
-      <button>Pulsa Aquí</button>
+      <Button text="Pulsa Aquí" />
     </div>
   );
 }
--- frontendxreact/src/components/Button.jsx
+++ frontendxreact/src/components/Button.jsx
@@ -1,8 +1,30 @@
-
-import React from "react";
+import React, { useState } from "react";
+import { getDatos } from "../services/API";
 
 const Button = ({ text }) => {
-    return <button>{text}</button>;
+    const [data, setData] = useState(null);
+
+    const handleClick = async () => {
+        try {
+            const result = await getDatos();
+            console.log('Datos obtenidos:', result);
+            setData(result);
+        } catch (error) {
+            console.error('Error al obtener los datos:', error);
+        }
+    };
+
+    return (
+        <div>
+            <button onClick={handleClick}>{text}</button>
+            {data && (
+                <div>
+                    <h2>Datos obtenidos:</h2>
+                    <pre>{JSON.stringify(data, null, 2)}</pre>
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default Button;
